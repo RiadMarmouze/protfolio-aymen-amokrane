d7b886798eedc5a23288edfcd0f6de662623edac
RiadMarmouze <riadmarmouze1@gmail.com>
2025-10-08 17:56:12 +0100

    Initial commit

--- src/app/(public)/collab/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import CollaborationViewer from "./viewer";
-export default async function CollabPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { id } = await params; 
-  return <CollaborationViewer id={id} />;
-}
